19d5bac5afcc27af91f8d20772210e10da0080f3
alan7s <alan7@outlook.com>
2023-12-20 21:28:49 -0300

    add support more Shodan info

--- js/script.js
+++ js/script.js
@@ -14,6 +14,9 @@ async function checkIP() {
     }
 
     try {
+        vtContainer.innerHTML = '<p><strong>Requesting VirusTotal...</strong></p>';
+        shodanContainer.innerHTML = '<p><strong>Requesting Shodan...</strong></p>';
+
         // Consulta a API do VirusTotal
         const virusTotalResponse = await fetch(`https://www.virustotal.com/api/v3/ip_addresses/${ipInput.value}`, {
             headers: {
@@ -28,11 +31,11 @@ async function checkIP() {
             const totalVendors = Object.keys(virusTotalData.data.attributes.last_analysis_results).length;
 
             // Exibe o resultado do VirusTotal na página com botão de acesso
-            vtContainer.innerHTML = `<p><strong>VirusTotal scan:</strong> <a href="https://www.virustotal.com/gui/ip-address/${ipInput.value}">${virusTotalData.data.attributes.last_analysis_stats.malicious}/${totalVendors}</a> security vendors flagged that IP address.</p>`;
+            vtContainer.innerHTML = `<p><strong>VirusTotal scan:</strong> <a href="https://www.virustotal.com/gui/ip-address/${ipInput.value}" target="_blank">${virusTotalData.data.attributes.last_analysis_stats.malicious}/${totalVendors}</a> security vendors flagged that IP address.</p>`;
         } else {
             const errorData = await virusTotalResponse.json();
             console.error('Error when requesting the VirusTotal API:', errorData.error.message);
-            vtContainer.innerHTML = `<p>${errorData.error.message}</p>`;
+            vtContainer.innerHTML = `<p><strong>VirusTotal scan:</strong>${errorData.error.message}</p>`;
         }
 
         // Consulta a API do Shodan
@@ -40,17 +43,21 @@ async function checkIP() {
 
         if (shodanResponse.ok) {
             const shodanData = await shodanResponse.json();
-
+            if (shodanData.domains.length > 3) {
+                const first3domains = shodanData.domains.slice(0, 4); // Limita a 3 domains o resultado
+                first3domains[3] = ` <a href="https://www.shodan.io/host/${ipInput.value}" target="_blank">and others</a>`;
+                shodanData.domains = first3domains;
+            }
             // Exibe o resultado do Shodan na página com botão de acesso
             if (shodanData.vulns) {
-                shodanContainer.innerHTML = `<p><strong>Shodan scan:</strong> <a href="https://www.shodan.io/host/${ipInput.value}">${shodanData.vulns.length}</a> vulnerabilities.`;
+                shodanContainer.innerHTML = `<p><strong>Shodan scan:</strong> <ul><li>Organization: ${shodanData.org}</li><li>Domains: ${shodanData.domains}</li><li>Ports: ${shodanData.ports}</li><li>Vulnerabilities: <a href="https://www.shodan.io/host/${ipInput.value}" target="_blank">${shodanData.vulns.length}</a></li></ul></p>`;
             } else {
-                shodanContainer.innerHTML = `<p><strong>Shodan scan:</strong> <a href="https://www.shodan.io/host/${ipInput.value}">0</a> vulnerabilities.</p>`;
+                shodanContainer.innerHTML = `<p><strong>Shodan scan:</strong> <ul><li>Organization: ${shodanData.org}</li><li>Domains: ${shodanData.domains}</li><li>Ports: ${shodanData.ports}</li><li>Vulnerabilities: <a href="https://www.shodan.io/host/${ipInput.value}" target="_blank">0</a></li></ul></p>`;
             }
         } else {
             const errorData = await shodanResponse.json();
-            console.error('Error in requesting the Shodan API:', errorData.error);
-            shodanContainer.innerHTML = `<p>${errorData.error}</p>`;
+            console.error('Error when requesting the Shodan API:', errorData.error);
+            shodanContainer.innerHTML = `<p><strong>Shodan scan:</strong> ${errorData.error}</p>`;
         }
 
     } catch (error) {
